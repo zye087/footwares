13178472b9caf2a3f2c3f5466528513daf00497a
Zye <gellertgrindelwald087@gmail.com>
2025-12-03 18:11:51 +0800

    First commit

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,13 @@
+<nav class="bg-slate-900 w-3/4 rounded-xl flex text-white justify-between items-center p-5">
+    <div>
+        <h1 class="text-red-500 font-bold tracking-widest text-lg">FOOTWARE</h1>
+    </div>
+    <div class="flex gap-5">
+        <a href="">Shop</a>
+        <a href="">Collections</a>
+    </div>
+    <div class="flex gap-5">
+        <p>Bag</p>
+        <p>Profile</p>
+    </div>
+</nav>
---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,10 @@
+<x-app>
+    <header class="p-2.5 flex justify-center">
+        <x-nav />
+    </header>
+    <main class="flex justify-center">
+        <div class="w-3/4 p-2.5">
+            <h1>Content here...</h1>
+        </div>
+    </main>
+</x-app>
