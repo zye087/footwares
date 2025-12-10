4b9340f03ec239bab72e730ec43053f4bb6b7480
Zye <gellertgrindelwald087@gmail.com>
2025-12-11 06:59:13 +0800

    12/11/25

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,11 +7,79 @@
         <div class="w-[85%] px-2.5">
             <x-carousel />
             
-            <div class="p-2.5">
-                <div class="w-[250px]">
-                    <img src="{{ asset("images/product_01.png") }}" alt="products">
-                    <p>sample</p>
-                    <p>₱ 1,000</p>
+            <div class="py-[100px] flex justify-center gap-4">
+                {{-- Row 1 --}}
+                <div class="w-[200px] shadow-xl rounded-xl">
+                    <img src="{{ asset("images/product(8).png") }}" alt="products" class="rounded-t-xl w-[200px] h-[250px]">
+                    <div class="p-4 flex flex-col gap-2">
+                        <p>Luka Jordan</p>
+                        <p>₱ 1,000</p>
+                    </div>
+                </div>
+                <div class="w-[200px] shadow-xl rounded-xl">
+                    <img src="{{ asset("images/product(8).png") }}" alt="products" class="rounded-t-xl w-[200px] h-[250px]">
+                    <div class="p-2.5">
+                        <p>Luka Jordan</p>
+                        <p>₱ 1,000</p>
+                    </div>
+                </div>
+                <div class="w-[200px] shadow-xl rounded-xl">
+                    <img src="{{ asset("images/product(8).png") }}" alt="products" class="rounded-t-xl w-[200px] h-[250px]">
+                    <div class="p-2.5">
+                        <p>Luka Jordan</p>
+                        <p>₱ 1,000</p>
+                    </div>
+                </div>
+                <div class="w-[200px] shadow-xl rounded-xl">
+                    <img src="{{ asset("images/product(8).png") }}" alt="products" class="rounded-t-xl w-[200px] h-[250px]">
+                    <div class="p-2.5">
+                        <p>Luka Jordan</p>
+                        <p>₱ 1,000</p>
+                    </div>
+                </div>
+                <div class="w-[200px] shadow-xl rounded-xl">
+                    <img src="{{ asset("images/product(8).png") }}" alt="products" class="rounded-t-xl w-[200px] h-[250px]">
+                    <div class="p-2.5">
+                        <p>Luka Jordan</p>
+                        <p>₱ 1,000</p>
+                    </div>
+                </div>
+
+                {{-- Row 2 --}}
+                <div class="w-[200px] shadow-xl rounded-xl">
+                    <img src="{{ asset("images/product(8).png") }}" alt="products" class="rounded-t-xl w-[200px] h-[250px]">
+                    <div class="p-4 flex flex-col gap-2">
+                        <p>Luka Jordan</p>
+                        <p>₱ 1,000</p>
+                    </div>
+                </div>
+                <div class="w-[200px] shadow-xl rounded-xl">
+                    <img src="{{ asset("images/product(8).png") }}" alt="products" class="rounded-t-xl w-[200px] h-[250px]">
+                    <div class="p-2.5">
+                        <p>Luka Jordan</p>
+                        <p>₱ 1,000</p>
+                    </div>
+                </div>
+                <div class="w-[200px] shadow-xl rounded-xl">
+                    <img src="{{ asset("images/product(8).png") }}" alt="products" class="rounded-t-xl w-[200px] h-[250px]">
+                    <div class="p-2.5">
+                        <p>Luka Jordan</p>
+                        <p>₱ 1,000</p>
+                    </div>
+                </div>
+                <div class="w-[200px] shadow-xl rounded-xl">
+                    <img src="{{ asset("images/product(8).png") }}" alt="products" class="rounded-t-xl w-[200px] h-[250px]">
+                    <div class="p-2.5">
+                        <p>Luka Jordan</p>
+                        <p>₱ 1,000</p>
+                    </div>
+                </div>
+                <div class="w-[200px] shadow-xl rounded-xl">
+                    <img src="{{ asset("images/product(8).png") }}" alt="products" class="rounded-t-xl w-[200px] h-[250px]">
+                    <div class="p-2.5">
+                        <p>Luka Jordan</p>
+                        <p>₱ 1,000</p>
+                    </div>
                 </div>
             </div>
         </div>
