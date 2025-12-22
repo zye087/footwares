cb1d69e7eae17fad25a09149d4dd118f1839e4ff
Zye <gellertgrindelwald087@gmail.com>
2025-12-22 17:12:46 +0800

    12/22/25

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -7,7 +7,15 @@
         <h1 class="text-amber-800 font-bold tracking-widest text-4xl hover:text-amber-700 transition duration-150 cursor-pointer">FOOTWARES</h1>
     </div>
     <div class="flex gap-4 text-gray-200">
-        <img src="{{ asset("images/shopping-bag.png") }}" alt="Bag" class="w-[22px] cursor-pointer">
-        <img src="{{ asset("images/account.png") }}" alt="Account" class="w-[22px] cursor-pointer">
+        <a href="" class="hover:text-amber-700 text-amber-800 transition duration-150 cursor-pointer font-bold">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+            <path fill-rule="evenodd" d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 0 0 4.25 22.5h15.5a1.875 1.875 0 0 0 1.865-2.071l-1.263-12a1.875 1.875 0 0 0-1.865-1.679H16.5V6a4.5 4.5 0 1 0-9 0ZM12 3a3 3 0 0 0-3 3v.75h6V6a3 3 0 0 0-3-3Zm-3 8.25a3 3 0 1 0 6 0v-.75a.75.75 0 0 1 1.5 0v.75a4.5 4.5 0 1 1-9 0v-.75a.75.75 0 0 1 1.5 0v.75Z" clip-rule="evenodd" />
+            </svg>
+        </a>
+        <a href="" class="hover:text-amber-700 text-amber-800 transition duration-150 cursor-pointer font-bold">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+            <path fill-rule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clip-rule="evenodd" />
+            </svg>
+        </a>
     </div>
 </nav>
